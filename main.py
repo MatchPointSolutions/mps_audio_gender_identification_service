e4785784732dd6033b9dc0baee54199fb637ce41
TejasChintalapudi <tejaswini.c@matchps.com>
2023-11-21 23:00:40 +0530

    commented check condition file type

--- main.py
+++ main.py
@@ -22,16 +22,18 @@ async def create_upload_file(file: UploadFile = File(...)):
 
 @app.post("/test_multiaudio_pyannote/")
 async def upload_file(file: UploadFile = File(...)):
-    if file.content_type not in ["audio/mp3", "audio/aac", "audio/wav"]:
-        return {"error": "Invalid file format. Please upload MP3 or WAV files."}
+    # if file.content_type not in ["audio/mp3", "audio/aac", "audio/wav"]:
+    #     return {"error": "Invalid file format. Please upload MP3 or WAV files."}
 
-    # Save the file
-    with open(file.filename, "wb") as f:
-        f.write(file.file.read())
-    logger.info(file.filename)
-    result = get_multi_voice_output(file.filename)
-    return {"filename": file.filename,
-            "result": result}
+    try:
+        with open(file.filename, "wb") as f:
+            f.write(file.file.read())
+        logger.info(file.filename)
+        result = get_multi_voice_output(file.filename)
+        return {"filename": file.filename,
+                "result": result}
+    except Exception as error:
+        return {"Error": str(error)}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, log_level="info")
