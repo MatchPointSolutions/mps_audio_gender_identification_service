79462e0d494856a2b1e3241a09cb13acb5c718da
TejasChintalapudi <tejaswini.c@matchps.com>
2023-11-21 17:09:04 +0530

    updated multi_voice_recognition_model

--- multi_voice_recognition_model.py
+++ multi_voice_recognition_model.py
@@ -7,26 +7,29 @@ from voice_recognition_model import identify_the_audio
 
 logger = setup_logger(__name__)
 
+
 def get_multi_voice_output(audio_file):
     pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1",use_auth_token=ACCESS_TOKEN)
-    #pipeline.to(torch.device("cuda"))
     diarization = pipeline(audio_file)
-    # Load audio file
     y, sr = librosa.load(audio_file)
     speaker_list = []
-    file_name_1="extracted_audio"
-    file_name_2 =".wav"
-    i=0
+    file_name_1 = "extracted_audio_"
+    file_name_2 = ".wav"
+
+    i = 0
     for turn, _, speaker in diarization.itertracks(yield_label=True):
-        logger.info(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
-        #speaker_list.append(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
         start_time = turn.start
         end_time = turn.end
         start_sample = int(start_time * sr)
         end_sample = int(end_time * sr)
+        start_sample = int(start_sample)
+        end_sample = int(end_sample)
         extracted_audio = y[start_sample:end_sample]
-        # Save extracted audio
-        librosa.save(file_name_1+i+file_name_2, extracted_audio, sr)
-        result = identify_the_audio(file_name_1+i+file_name_2)
-        i=i+1
-    return speaker_list.append(result)
+        extracted_audio_file = f"{file_name_1}{i}{file_name_2}"
+        librosa.output.write_wav(extracted_audio_file, extracted_audio, sr)
+        result = identify_the_audio(extracted_audio_file)
+        print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker} Identified Speakers: {result}")
+        speaker_list.extend(result)
+        i += 1
+
+    return speaker_list
