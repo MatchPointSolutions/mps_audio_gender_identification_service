378e2d00d77ebf195762bb3955eb8b9ee74a822d
Sudhakar <sudhakar.k@matchps.com>
2023-12-26 20:56:19 +0530

    updated deployment location

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM mpsacr.azurecr.io/myapp-base:spleeter
 
 WORKDIR /app
+COPY env_from_github_secrets .env
 COPY . .
 EXPOSE 7860
 # CMD ["uvicorn", "test:app", "--host", "0.0.0.0", "--port", "7860"]
--- log.py
+++ log.py
@@ -3,16 +3,23 @@ Logger Setup
 """
 import logging
 from config import LOG_FILE_PATH
+import sys
 
 
-def setup_logger(logger_name, log_file=LOG_FILE_PATH, level=logging.INFO):
+
+
+def setup_logger(logger_name, level=logging.INFO):
     """
     Setup the logger and return the logger object.
     """
     formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
-    handler = logging.FileHandler(log_file)
+
+    # Use StreamHandler to send log messages to stdout
+    handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
+
     logger = logging.getLogger(logger_name)
     logger.setLevel(level)
     logger.addHandler(handler)
+
     return logger
